694a00c1f333c03b1f017cbce6c6d8e67d7cc02d
Andrew <andrewryanogle@gmail.com>
2019-05-18 13:06:20 -0700

    added some styling to the contact form

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
-import axios from 'axios';
+import axios from "axios";
+import styled from "styled-components";
+import { Transition } from "react-spring/renderprops";
+
 class Contact extends Component {
   constructor(props) {
     super(props);
@@ -11,87 +14,118 @@ class Contact extends Component {
       buttonText: "Send Message"
     };
   }
-  formSubmit = (e) => {
-    e.preventDefault()
-  
+  formSubmit = e => {
+    e.preventDefault();
+
     this.setState({
-        buttonText: '...sending'
-    })
-  
+      buttonText: "...sending"
+    });
+
     let data = {
-        name: this.state.name,
-        email: this.state.email,
-        message: this.state.message
-    }
-    
-    axios.post('https://andrew-ogle-contact-form.herokuapp.com/api/v1', data)
-    .then( res => {
-        this.setState({ sent: true }, this.resetForm())
-    })
-    .catch( () => {
+      name: this.state.name,
+      email: this.state.email,
+      message: this.state.message
+    };
+
+    axios
+      .post("https://andrew-ogle-contact-form.herokuapp.com/api/v1", data)
+      .then(res => {
+        this.setState({ sent: true }, this.resetForm());
+      })
+      .catch(() => {
         this.setState({
-            name: '',
-            message: '',
-            email: '',
-            buttonText: 'Message Not Sent'
-        })
-    })
-  }
+          name: "",
+          message: "",
+          email: "",
+          buttonText: "Message Not Sent"
+        });
+      });
+  };
   resetForm = () => {
     this.setState({
-        name: '',
-        message: '',
-        email: '',
-        buttonText: 'Message Sent'
-    })
-}
+      name: "",
+      message: "",
+      email: "",
+      buttonText: "Message Sent"
+    });
+  };
   render() {
+    const Container = styled.div`
+      width: 60%;
+      font-family: "Crimson Text", serif;
+      padding: 33px;
+      background-color: #efefea;
+      font-size: 16px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      form{
+        display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      }
+    `;
     return (
-      <form className="contact-form" onSubmit={e => this.formSubmit(e)}>
-        <label htmlFor="message-input">
-          Your Message
-        </label>
-        <textarea
-          onChange={e => this.setState({ message: e.target.value })}
-          name="message"
-          class="message-input"
-          type="text"
-          placeholder="Please write your message here"
-          value={this.state.message}
-          required
-        />
+      <Container>
+        <Transition
+          items={Contact}
+          from={{ opacity: 0, transform: "translate3d(100%,0,0)" }}
+          enter={{ opacity: 1, transform: "translate3d(0%,0,0)" }}
+          leave={{ opacity: 0, transform: "translate3d(-50%,0,0)" }}
+        >
+          {show =>
+            show &&
+            (props => (
+              <div style={props}>
+                <form
+                  className="contact-form"
+                  onSubmit={e => this.formSubmit(e)}
+                >
+                  <label htmlFor="message-input">Your Message</label>
+                  <textarea
+                    onChange={e => this.setState({ message: e.target.value })}
+                    name="message"
+                    class="message-input"
+                    type="text"
+                    placeholder="Please write your message here"
+                    value={this.state.message}
+                    required
+                  />
 
-        <label class="message-name" htmlFor="message-name">
-          Your Name
-        </label>
-        <input
-          onChange={e => this.setState({ name: e.target.value })}
-          name="name"
-        
-          type="text"
-          placeholder="Your Name"
-          value={this.state.name}
-        />
+                  <label class="message-name" htmlFor="message-name">
+                    Your Name
+                  </label>
+                  <input
+                    onChange={e => this.setState({ name: e.target.value })}
+                    name="name"
+                    type="text"
+                    placeholder="Your Name"
+                    value={this.state.name}
+                  />
 
-        <label  htmlFor="message-email">
-          Your Email
-        </label>
-        <input
-          onChange={e => this.setState({ email: e.target.value })}
-          name="email"
-          
-          type="email"
-          placeholder="your@email.com"
-          required
-          value={this.state.email}
-        />
+                  <label htmlFor="message-email">Your Email</label>
+                  <input
+                    onChange={e => this.setState({ email: e.target.value })}
+                    name="email"
+                    type="email"
+                    placeholder="your@email.com"
+                    required
+                    value={this.state.email}
+                  />
 
-        <div className="button--container">
-          <button type="submit" className="button button-primary">
-            {this.state.buttonText}
-          </button>
-        </div>
-      </form>
+                  <div className="button--container">
+                    <button type="submit" className="button button-primary">
+                      {this.state.buttonText}
+                    </button>
+                  </div>
+                </form>
+              </div>
+            ))
+          }
+        </Transition>
+      </Container>
     );
   }
 }
--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import {Transition} from "react-spring/renderprops";
+import { Transition } from "react-spring/renderprops";
 
 const Home = () => {
   const Container = styled.div`
@@ -17,20 +17,30 @@ const Home = () => {
   return (
     <Container>
       <Transition
-  items={Home}
-  from={{ opacity: 0, transform: 'translate3d(100%,0,0)' }}
-  enter={{ opacity: 1, transform: 'translate3d(0%,0,0)' }}
-  leave={{ opacity: 0, transform: 'translate3d(-50%,0,0)' }}>
-  {show => show && (props => <div style={props}>
-            <h1>Hello There</h1>
-            <p>
-              I am Andrew Ogle, a full stack web developer, and lover of all
-              things nerdy. You can usually find me either coding or drawing. I
-              love making cool things, and working with creative people. Contact
-              me if you would like to make something awesome together.
-            </p>
-          </div>
-        )}
+        items={Home}
+        from={{ opacity: 0, transform: "translate3d(100%,0,0)" }}
+        enter={{ opacity: 1, transform: "translate3d(0%,0,0)" }}
+        leave={{ opacity: 0, transform: "translate3d(-50%,0,0)" }}
+      >
+        {show =>
+          show &&
+          (props => (
+            <div style={props}>
+              <h1>Hello There</h1>
+              <p>
+                I am Andrew Ogle, a full stack web developer, and lover of all
+                things nerdy. You can usually find me either coding or drawing.
+                I love making cool things, and working with creative people.
+                Contact me if you would like to make something awesome together.
+              </p>
+              <p>
+                This Website was built using React, React-Router-Dom,React
+                Spring, and Styled Components. For the contact form I used
+                Axios, Node, Nodemailer, Express, and Body Parser.
+              </p>
+            </div>
+          ))
+        }
       </Transition>
     </Container>
   );
